e31c423aa562d88d6817ef5c38512e164f80a37a
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-03-06 15:16:18 -0500

    commit tree e29585a38ab7fcffebb9ba3ec2321f27e1e438c1

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
 
-  // replace 'false' with a state variable that can be toggled between true and false
+  const [isDarkMode, setIsOn] = useState(false); 
+  
+  // replace 'false' with a state variable (above) that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = isDarkMode ? "App dark" : "App light"
+
+  
+  //event handler => update state when button is clicked (see onClick below)
+  function handleDarkModeButton() {
+    setIsOn((isDarkMode) => !isDarkMode);
+  } 
+
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleDarkModeButton}>Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [isInCart, setIsInCart] = useState(false);
+
+  const cartState = isInCart ? "in-cart" :  "";
+  //or set className directly: className={isInCart? "in-cart" : ""}
+
+  function handleAddToCart() {
+    setIsInCart((isInCart) => !isInCart);
+  }
+
   return (
-    <li className="">
+    <li className={cartState}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button 
+        className={isInCart ? "Remove From Cart" : "Add to Cart"}
+        onClick={handleAddToCart}
+      >
+        {isInCart ? "Remove From" : "Add to"} Cart
+      </button>
     </li>
   );
 }
